e24a0ff3645ff88cfa1b0df0342fd3f1bcd6c7d9
Bryan Stoduto <stodutolucas1@gmail.com>
2026-06-30 19:35:09 -0300

    function dolist

---
+++ script.js
@@ -0,0 +1,15 @@
+const formEl = document.querySelector(".form");
+
+const inputEl = document.querySelector(".input");
+
+formEl.addEventListener("submit", (event)=>{
+event.preventDefault();
+console.log(inputEl.value);
+todoList()
+
+
+});
+
+function toDoList(){
+
+}
