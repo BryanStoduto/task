1c8986781d19a73edd83506c7e731407f9c9c7b8
Bryan Stoduto <stodutolucas1@gmail.com>
2026-06-30 19:46:50 -0300

    todoAdd

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ const formEl = document.querySelector(".form");
 
 const inputEl = document.querySelector(".input");
 
-formEl.addEventListener("submit", (event)=>{
-event.preventDefault();
-console.log(inputEl.value);
-todoList()
+const ulEl= document.querySelector(".list");
 
+formEl.addEventListener("submit", (event) =>
+
+{
+event.preventDefault();
+toDoList();
 
 });
 
 function toDoList(){
+    let newTask = inputEl.value;
+    const liEl = document.createElement("li");
+    liEl.innerText = newTask;
+    ulEl.appendChild(liEl)
 
 }
